dbb89fe311209983c58babeb4153e0091fe1f72d
Johnny_GM <42081527+gomezmarinjohnny@users.noreply.github.com>
2018-08-10 20:00:50 -0500

    primer ensayo
    first commit in class

---
+++ proyecto1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+	int a,b,c, mayor, menor;
+	float div;
+	cout<< "  Ingrese el valor de a:   ";
+	cin>>a;
+	cout << "  Ingrese el valor de b:   ";
+	cin>>b;
+	cout<< "  Ingrese el valor de c:   ";
+	cin>>c;
+	if((a>b)&&(b>c))
+	{
+	mayor=a; menor=c;
+	cout<< "  El valor mayor es " <<a<< " y el valor menor es  " <<c<<"\n";
+    }
+	if((b>a)&&(c>a))
+	{
+	mayor=b; menor=a;
+	cout<< "  El valor mayor es " <<b<< " y el valor menor es  " <<a<<"\n";
+    }
+	if((c>a)&&(a>b))
+	{
+	mayor=c; menor=b;
+	cout<< "  El valor mayor es " <<c<< " y el valor menor es  " <<b<<"\n";
+    }
+    div=mayor/menor;
+    cout<< "  La division entre: " <<mayor<< " y :  " <<menor<< "  es = " <<div<<"\n";
+	return 0;
+}
